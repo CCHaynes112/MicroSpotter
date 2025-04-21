40366b5a56deea94e3d567180a258301be302991
Curtis Haynes <CCHaynes1122@gmail.com>
2025-04-20 21:33:22 -0700

    Random changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import firebaseui from "firebaseui";
+import firebase from "firebase/compat/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +23,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
+export const auth = getAuth(app);
+
+// FirebaseUI config
+const ui = new firebaseui.auth.AuthUI(auth);
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
+});
--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
+import firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+import { auth } from '../firebase';
 
 interface LoginFormProps {
     onLogin: (username: string, password: string) => void;
@@ -9,41 +13,54 @@ export default function LoginForm({ onLogin }: LoginFormProps) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        onLogin(username, password);
-    };
+    const SignIn = () => {
+        useEffect(() => {
+            const ui = new firebaseui.auth.AuthUI(auth);
 
-    return (
-        <Box component="form" onSubmit={handleSubmit} sx={{ maxWidth: 300, margin: 'auto', mt: 4 }}>
-            <Typography variant="h4" component="h1" gutterBottom>
-                Login to MicroSpotter
-            </Typography>
-            <TextField
-                fullWidth
-                margin="normal"
-                label="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-            />
-            <TextField
-                fullWidth
-                margin="normal"
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-            >
-                Log In
-            </Button>
-        </Box>
-    );
-}
+            ui.start('#firebaseui-auth-container', {
+                signInOptions: [
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                ],
+                signInSuccessUrl: '/',
+            });
+        }, []);
+
+
+        const handleSubmit = (e: React.FormEvent) => {
+            e.preventDefault();
+            onLogin(username, password);
+        };
+
+        return (
+            <Box component="form" onSubmit={handleSubmit} sx={{ maxWidth: 300, margin: 'auto', mt: 4 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Login to MicroSpotter
+                </Typography>
+                <TextField
+                    fullWidth
+                    margin="normal"
+                    label="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <TextField
+                    fullWidth
+                    margin="normal"
+                    label="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Log In
+                </Button>
+            </Box>
+        );
+    }
